5183cc7e706cd3cf396a4b4bb8135708cfb3d18a
Allan <270697583@yoobeestudent.ac.nz>
2026-07-25 14:46:44 +1200

    added / configured RDS eb DB config

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import Flask,render_template,request, redirect
 from models import db, InfoModel
-
 from flask import Flask
 import os
  
@@ -21,8 +20,7 @@ if 'RDS_DB_NAME' in os.environ:
     )
 else:
     # our database uri
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/flaskdemodb"
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:something@localhost:5432/flaskdemodb"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
